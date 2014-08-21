7464a4e804b90527ff5d000becf0dbf95ccf3ce7
Paul Jimenez <pj@place.org>
2014-08-21 13:32:52 -0400

    Fixed the bookmarklet to submit correctly
    Also submit the title as well as the URL

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:
-    data=JSON.stringify({"payload":location.href});
-    url="https://huginn.in.safernet.link/users/1/web_requests/2/incoming_link_secret_string";
+    data=JSON.stringify({"url":location.href, "title":document.title});
+    url="http://huginn.in.safernet.link/users/1/web_requests/2/incoming_link_secret_string";
 
     f     = document.createElement('iframe');
     fname = (+((''+Math.random()).substring(2))).toString(36);
@@ -16,7 +16,7 @@ javascript:
         form  = doc.createElement('form');
         text  = doc.createElement('textarea');
 
-    text.setAttribute('name', 'data');
+    text.setAttribute('name', 'payload');
     text.appendChild(doc.createTextNode(data));
 
     form.setAttribute('action', url);
